d7594e84de2d8c00666eb486b2c16b16f43351c5
Matthias De Vriendt <devriendt.matthias@gmail.com>
2025-10-08 16:21:33 +0200

    fix: rename NEX_WORKLOAD_NATS_URL to NEX_WORKLOAD_NATS_SERVERS

--- runtime/env.go
+++ runtime/env.go
@@ -1,13 +1,13 @@
 package runtime
 
 const (
-    NatsUrlVar  = "NEX_WORKLOAD_NATS_URL"
-    NatsJwtVar  = "NEX_WORKLOAD_NATS_B64_JWT"
-    NatsSeedVar = "NEX_WORKLOAD_NATS_NKEY"
+	NatsUrlVar  = "NEX_WORKLOAD_NATS_SERVERS"
+	NatsJwtVar  = "NEX_WORKLOAD_NATS_B64_JWT"
+	NatsSeedVar = "NEX_WORKLOAD_NATS_NKEY"
 
-    GroupEnvVar     = "NEX_WORKLOAD_GROUP"
-    NamespaceEnvVar = "NEX_WORKLOAD_NAMESPACE"
-    InstanceEnvVar  = "NEX_WORKLOAD_ID"
+	GroupEnvVar     = "NEX_WORKLOAD_GROUP"
+	NamespaceEnvVar = "NEX_WORKLOAD_NAMESPACE"
+	InstanceEnvVar  = "NEX_WORKLOAD_ID"
 
-    LogLevelEnvVar = "CONNECT_LOG_LEVEL"
+	LogLevelEnvVar = "CONNECT_LOG_LEVEL"
 )
